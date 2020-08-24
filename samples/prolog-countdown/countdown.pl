b2b9b7977c3a49acf9fedc7418165e3043f6a085
Frederic Vogels <frederic.vogels@ucll.be>
2020-08-24 15:31:03 +0200

    countdown

--- samples/prolog-countdown/countdown.pl
+++ samples/prolog-countdown/countdown.pl
@@ -14,14 +14,23 @@ solve(Ns, Goal) :-
     ),
     solve([Z|R2], Goal).
 
-solve(Ns, Goal, []) :- member(Goal, Ns).
-solve(Ns, Goal, [Step|History]) :-
+
+evaluate(+, X, Y, R) :- R is X + Y.
+evaluate(-, X, Y, R) :- R is X - Y.
+evaluate(*, X, Y, R) :- R is X * Y.
+evaluate(/, X, Y, R) :- Y =\= 0, 0 is mod(X, Y), R is div(X, Y).
+
+value(X, X) :- number(X).
+value((Op, X, Y), R) :-
+    value(X, XV),
+    value(Y, YV),
+    evaluate(Op, XV, YV, R).
+
+solve2(Ns, Goal, N) :-
+    member(N, Ns),
+    value(N, Goal).
+solve2(Ns, Goal, Solution) :-
     pick(Ns, X, R),
     pick(R, Y, R2),
-    (
-    	Z is X + Y, Step = add(X,Y)
-    ;   Z is X - Y, Step = sub(X,Y)
-    ;   Z is X * Y, Step = mul(X,Y)
-    ;   Y =\= 0, 0 is mod(X, Y), Z is div(X, Y), Step=div(X,Y)
-    ),
-    solve([Z|R2], Goal, History).
+    member(Op, [+, -, *, /]),
+    solve2([(Op, X, Y) | R2], Goal, Solution).
