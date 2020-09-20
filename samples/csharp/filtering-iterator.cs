ad1db9ffe390a7b0ac19e205baa81995fb84d663
Frederic Vogels <frederic.vogels@ucll.be>
2020-09-20 19:34:56 +0200

    csharp/filtering-iterator

---
+++ samples/csharp/filtering-iterator.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorDemo
+{
+    interface Iterator<T>
+    {
+        T Current { get; }
+        bool HasNext { get; }
+
+        void Next();
+    }
+
+    class RangeIterator : Iterator<int>
+    {
+        private int current;
+
+        private int maximum;
+
+        public RangeIterator(int from, int to)
+        {
+            this.current = from;
+            this.maximum = to;
+        }
+
+        public int Current => current;
+
+        public bool HasNext => current < maximum;
+
+        public void Next()
+        {
+            ++current;
+        }
+    }
+
+    class FilteringIterator<T> : Iterator<T>
+    {
+        private Iterator<T> iterator;
+
+        private Predicate<T> predicate;
+
+        private T current;
+
+        public FilteringIterator(Iterator<T> iterator, Predicate<T> predicate)
+        {
+            this.iterator = iterator;
+            this.predicate = predicate;
+        }
+
+        public T Current => current;
+
+        public bool HasNext => iterator != null;
+
+        public void Next()
+        {
+            current = iterator.Current;
+
+            AdvanceToNext();
+        }
+
+        private void AdvanceToNext()
+        {
+            if (!iterator.HasNext)
+            {
+                iterator = null;
+            }
+            else
+            {
+                iterator.Next();
+
+                while (!predicate(iterator.Current))
+                {
+                    if (!iterator.HasNext)
+                    {
+                        iterator = null;
+                        return;
+                    }
+
+                    iterator.Next();
+                }
+            }
+        }
+    }
+}
