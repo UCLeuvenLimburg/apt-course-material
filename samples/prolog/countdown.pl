a95dd64faf4714b1d178742e789c792f8a493d0f
Frederic Vogels <frederic.vogels@ucll.be>
2020-09-26 17:25:11 +0200

    prolog/countdown

--- samples/prolog/countdown.pl
+++ samples/prolog/countdown.pl
@@ -1,15 +1,17 @@
-solve(Ns, Goal) :- member(Goal, Ns).
-solve(Ns, Goal) :-
+operation(X, Y, R) :- R is X + Y.
+operation(X, Y, R) :- X > Y, R is X - Y.
+operation(X, Y, R) :- X =\= 0, Y =\= 0, R is X * Y.
+operation(X, Y, R) :- Y =\= 0, 0 is mod(X, Y), R is div(X, Y).
+
+reachable(Ns, Goal) :- member(Goal, Ns).
+reachable(Ns, Goal) :-
     select(X, Ns, R),
     select(Y, R, R2),
-    (
-    	Z is X + Y
-    ;   Z is X - Y
-    ;   Z is X * Y
-    ;   Y =\= 0, 0 is mod(X, Y), Z is div(X, Y)
-    ),
-    solve([Z|R2], Goal).
+    operation(X, Y, Z),
+    reachable([Z|R2], Goal).
 
+reachables(Ns, Goals) :-
+    setof(Goal, reachable(Ns, Goal), Goals).
 
 evaluate(+, X, Y, R) :- R is X + Y.
 evaluate(-, X, Y, R) :- R is X - Y.
@@ -22,11 +24,11 @@ value((Op, X, Y), R) :-
     value(Y, YV),
     evaluate(Op, XV, YV, R).
 
-solve2(Ns, Goal, N) :-
+solve(Ns, Goal, N) :-
     member(N, Ns),
     value(N, Goal).
-solve2(Ns, Goal, Solution) :-
+solve(Ns, Goal, Solution) :-
     select(X, Ns, R),
     select(Y, R, R2),
     member(Op, [+, -, *, /]),
-    solve2([(Op, X, Y) | R2], Goal, Solution).
+    solve([(Op, X, Y) | R2], Goal, Solution).
