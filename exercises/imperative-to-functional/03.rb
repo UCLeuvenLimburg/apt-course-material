6080a97ae00d3152a5779f350095f1a91376a3c7
Frederic Vogels <frederic.vogels@ucll.be>
2020-11-08 22:57:23 +0100

    03

---
+++ exercises/imperative-to-functional/03.rb
@@ -0,0 +1,26 @@
+require 'json'
+
+DATA = JSON.parse(STDIN.read)
+
+
+rid, = ARGV
+timeslots = []
+result = []
+
+for exam in DATA
+    if exam['student_id'] == rid
+        timeslot = exam['timeslot']
+
+        if timeslots.include? timeslot
+            result.push(timeslot) unless result.include?(timeslot)
+        else
+            timeslots.push(timeslot)
+        end
+    end
+end
+
+result.sort!
+
+for timeslot in result
+    puts timeslot
+end
