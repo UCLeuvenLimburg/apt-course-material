4771e80fd848883c6f99b6daec6f9289d71010ce
Frederic Vogels <frederic.vogels@ucll.be>
2020-11-08 22:57:18 +0100

    02

---
+++ exercises/imperative-to-functional/02.rb
@@ -0,0 +1,17 @@
+require 'json'
+
+DATA = JSON.parse(STDIN.read)
+
+
+rid, = ARGV
+result = []
+
+for exam in DATA
+    result.push(exam['course']) if exam['student_id'] == rid
+end
+
+result.sort!
+
+for course in result
+    puts course
+end
