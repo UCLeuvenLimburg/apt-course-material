bb04f9d7eccb41a8d9bbf1fbdbb9311497d76419
Frederic Vogels <frederic.vogels@ucll.be>
2020-12-05 21:48:32 +0100

    csharp/prime-generator

---
+++ samples/csharp/prime-generator/Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Linq.Enumerable;
+
+namespace Demo
+{
+    class Program
+    {
+            public static bool IsPrime(int n)
+            {
+                return n >= 2 && Range(2, n - 2).All(k => n % k != 0);
+            }
+
+            public static IEnumerable<int> Primes()
+            {
+                var n = 2;
+
+                while ( true )
+                {
+                    if (IsPrime(n))
+                    {
+                        yield return n;
+                    }
+
+                    n++;
+                }
+            }
+
+        static void Main(string[] args)
+        {
+            foreach ( var prime in Primes() )
+            {
+                Console.WriteLine(prime);
+            }
+        }
+    }
+}
