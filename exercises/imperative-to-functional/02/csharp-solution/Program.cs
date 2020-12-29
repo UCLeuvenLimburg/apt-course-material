eee38a7250edc15b02bc1aa40c7edd98a9f371e2
Frederic Vogels <frederic.vogels@ucll.be>
2020-12-29 20:36:39 +0100

    02 C# solution

---
+++ exercises/imperative-to-functional/02/csharp-solution/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace csharp_solution
+{
+    class Data
+    {
+        public List<Exam> exams { get; set; }
+    }
+
+    class Exam
+    {
+        public string student_id { get; set; }
+        public string course { get; set; }
+        public int timeslot { get; set; }
+        public string location { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rawData = File.ReadAllText(args[0]);
+            var rid = args[1];
+            var data = JsonSerializer.Deserialize<Data>(rawData);
+            var result = data.exams.Where(exam => exam.student_id == rid).Select(exam => exam.course).OrderBy(x => x);
+
+            Console.WriteLine(string.Join("\n", result));
+        }
+    }
+}
