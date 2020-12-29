a9399e00505eb49e1d70573e95477dc768c5a5c9
Frederic Vogels <frederic.vogels@ucll.be>
2020-12-29 21:46:22 +0100

    cleanup

--- exercises/imperative-to-functional/03/example-solution.py
+++ exercises/imperative-to-functional/03/example-solution.py
@@ -3,13 +3,10 @@ import json
 import sys
 
 
-
-
 data = json.loads(sys.stdin.read())
 rid = sys.argv[1]
 
 frequencies = collections.Counter( exam['timeslot'] for exam in data['exams'] if exam['student_id'] == rid )
 result = sorted(timeslot for timeslot, frequency in frequencies.items() if frequency > 1)
-
 
 print("\n".join(str(x) for x in result))
