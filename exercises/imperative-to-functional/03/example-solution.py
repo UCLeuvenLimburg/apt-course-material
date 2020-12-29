7ef62595f59d8a758c4cfffa7442cc0f40faa35d
Frederic Vogels <frederic.vogels@ucll.be>
2020-12-29 21:46:08 +0100

    03 python solution

---
+++ exercises/imperative-to-functional/03/example-solution.py
@@ -0,0 +1,15 @@
+import collections
+import json
+import sys
+
+
+
+
+data = json.loads(sys.stdin.read())
+rid = sys.argv[1]
+
+frequencies = collections.Counter( exam['timeslot'] for exam in data['exams'] if exam['student_id'] == rid )
+result = sorted(timeslot for timeslot, frequency in frequencies.items() if frequency > 1)
+
+
+print("\n".join(str(x) for x in result))
